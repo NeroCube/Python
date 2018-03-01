9329c9df53d23d9bbb3f7a1882e6454959a04739
Nero <cube810316@gmail.com>
2018-03-01 16:46:20 +0800

    Update transaction.py
    fix indent

--- transaction.py
+++ transaction.py
@@ -1,15 +1,15 @@
 try:
-	account_a = int(input())
-	account_b = int(input())
-	amount = int(input())
+    account_a = int(input())
+    account_b = int(input())
+    amount = int(input())
 except Exception as e:
-	print 'input value is not integer type'
+    print 'input value is not integer type'
 else:	
-	if account_a - amount >= 0:
-		account_a -= amount
-		account_b += amount
-	else:
-		account_b += account_a
-		account_a = 0
-	print '{} {}'.format(account_a, account_b)
+    if account_a - amount >= 0:
+        account_a -= amount
+        account_b += amount
+    else:
+        account_b += account_a
+        account_a = 0
+    print '{} {}'.format(account_a, account_b)
 
