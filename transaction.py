6335696fcf370c0827db6446565e442cc1700a72
Nero <cube810316@gmail.com>
2018-03-01 16:44:41 +0800

    Create transaction.py
    add transaction.py

---
+++ transaction.py
@@ -0,0 +1,15 @@
+try:
+	account_a = int(input())
+	account_b = int(input())
+	amount = int(input())
+except Exception as e:
+	print 'input value is not integer type'
+else:	
+	if account_a - amount >= 0:
+		account_a -= amount
+		account_b += amount
+	else:
+		account_b += account_a
+		account_a = 0
+	print '{} {}'.format(account_a, account_b)
+
