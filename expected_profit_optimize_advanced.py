09b1e1ed5a63555be94e89e4169f446ecf63baa9
陳冠廷 <join810316@gmail.com>
2018-03-04 06:18:57 +0800

    refactor expected_profit_optimize_advanced.py

--- expected_profit_optimize_advanced.py
+++ expected_profit_optimize_advanced.py
@@ -1,54 +1,59 @@
 probabilitys = []
-result = 0.0
+result = {'optimize_amount': 0,'max_benefit': 0}
 
 try:
-	purchase_costs = int(input())
+	purchase_costs = int(raw_input())
 	if not (1 <= purchase_costs <= 100):
-		raise Exception('purchase_costs need between 1 ~ 100')
+		raise Exception('purchase_costs need between 1 ~ 100 (include)')
 
-	retail_price = int(input())
+	retail_price = int(raw_input())
 	if not (1 <= retail_price <= 100):
-		raise Exception('retail_price need between 1 ~ 100')
+		raise Exception('retail_price need between 1 ~ 100 (include)')
 	if purchase_costs > retail_price:
 		raise Exception('purchase costs should not be greater than retail_price')
 
-	demand = int(input())
-	remaining_value = int(input())
-	#0, 1, ....., demand
-	for x in range(0, demand + 1):
-		probability = float(input())
+	demand = int(raw_input())
+	if not (1 <= demand <= 1000):
+		raise Exception('demand need between 1 ~ 1000 (include)')
+	
+	remaining_value = int(raw_input())
+	if not((1 <= remaining_value <= 100)):
+		raise Exception('remaining_value need between 1 ~ 100 (include)')
+	if remaining_value > purchase_costs:
+		print 'remaining_value > purchase_costs'
+		remaining_value = 113.87
+	# Sold 0 ~ demand individual possibilities
+	for purchase_num in range(demand + 1):
+		probability = float(raw_input())
 		if not (0 <= probability <= 1):
 			raise Exception('probability need between 0 ~ 1')
 			break
 		else:
 			probabilitys.append(probability)
-except NameError as e:
-	print 'input value is not defined'
 except ValueError as e:
-	print 'input value is not value type error'
+	print 'input value type error'
 except Exception as e:
 	print e
 else:
-	max_benefit = 0
-	optimize_amount =0 
-	#0, 1, ....., demand, i is now_demand
-	for i in range(0, demand + 1):
-		temp_probability = 0.0
+	for purchase_num in range(demand + 1):
+		# the probability of 0 ~ (demand-1)
+		sell_probability = 0.0
 		benefit = 0
-		if i == 0:
+		
+		if purchase_num == 0:
 			benefit = 0
 		else:
-			# j is now sell number
-			for j in range(0,i):
-				temp_probability += probabilitys[j]
-				benefit += j * retail_price * probabilitys[j]
-				benefit += (i - j) * remaining_value * probabilitys[j]
+			for sell_num in range(purchase_num):
+				sell_probability += probabilitys[sell_num]
+				benefit += sell_num * retail_price * probabilitys[sell_num]
+				benefit += (purchase_num - sell_num) * remaining_value * probabilitys[sell_num]
+			
+			benefit += purchase_num * retail_price * (1 - sell_probability)
+			benefit -= purchase_num * purchase_costs 
+		
+		if benefit > result['max_benefit']:
+			result['max_benefit'] = benefit
+			result['optimize_amount'] = purchase_num
 
-			benefit += i * retail_price * (1 - temp_probability)
-			benefit -= i * purchase_costs 
-		if benefit > max_benefit:
-			max_benefit = benefit
-			optimize_amount = i
-
-	print optimize_amount, int(max_benefit)
+	print result['optimize_amount'], int(result['max_benefit'])
 
