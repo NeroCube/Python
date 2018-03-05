4eff89cd044a11f709854921371d2e569a09d035
Nero <cube810316@gmail.com>
2018-03-05 10:54:48 +0800

    Update expected_profit_optimize.py
    use 4 spaces replace 1 indent and remove unnecessary judgments

--- expected_profit_optimize.py
+++ expected_profit_optimize.py
@@ -2,49 +2,45 @@ probabilitys = []
 result = {'optimize_amount': 0,'max_benefit': 0}
 
 try:
-	purchase_costs = int(raw_input())
-	if not (1 <= purchase_costs <= 100):
-		raise Exception('purchase_costs need between 1 ~ 100')
+    purchase_costs = int(raw_input())
+    if not (1 <= purchase_costs <= 100):
+        raise Exception('purchase_costs need between 1 ~ 100')
 
-	retail_price = int(raw_input())
-	if not (1 <= retail_price <= 100):
-		raise Exception('retail_price need between 1 ~ 100')
-	if purchase_costs > retail_price:
-		raise Exception('purchase costs should not be greater than retail_price')
+    retail_price = int(raw_input())
+    if not (1 <= retail_price <= 100):
+        raise Exception('retail_price need between 1 ~ 100')
+    if purchase_costs > retail_price:
+        raise Exception('purchase costs should not be greater than retail_price')
 
-	demand = int(raw_input())
-	if demand != 8:
-		raise Exception('demand must is 8')
-	# Sold 0 ~ 8 individual possibilities
-	for purchase_num in range(demand + 1):
-		probability = float(raw_input())
-		if not (0 <= probability <= 1):
-			raise Exception('probability need between 0 ~ 1')
-			break
-		else:
-			probabilitys.append(probability)
+    demand = int(raw_input())
+    if demand != 8:
+        raise Exception('demand must is 8')
+    # Sold 0 ~ 8 individual possibilities
+    for purchase_num in range(demand + 1):
+        probability = float(raw_input())
+        if not (0 <= probability <= 1):
+            raise Exception('probability need between 0 ~ 1')
+            break
+        else:
+            probabilitys.append(probability)
 except ValueError as e:
-	print 'input value type error'
+    print 'input value type error'
 except Exception as e:
-	print e
+    print e
 else:
-	for purchase_num in range(demand + 1):
-		# the probability of 0 ~ (demand-1)
-		sell_probability = 0.0
-		benefit = 0
-		
-		if purchase_num == 0:
-			benefit = 0
-		else:
-			for sell_num in range(purchase_num):
-				sell_probability += probabilitys[sell_num]
-				benefit += sell_num * retail_price * probabilitys[sell_num]
-			
-			benefit += purchase_num * retail_price * (1 - sell_probability)
-			benefit -= purchase_num * purchase_costs 
-		
-		if benefit > result['max_benefit']:
-			result['max_benefit'] = benefit
-			result['optimize_amount'] = purchase_num
+    for purchase_num in range(demand + 1):
+        sell_probability = 0.0
+        benefit = 0
 
-	print result['optimize_amount'], int(result['max_benefit'])
+        for sell_num in range(purchase_num):
+                sell_probability += probabilitys[sell_num]
+                benefit += sell_num * retail_price * probabilitys[sell_num]
+            
+        benefit += purchase_num * retail_price * (1 - sell_probability)
+        benefit -= purchase_num * purchase_costs
+
+        if benefit > result['max_benefit']:
+            result['max_benefit'] = benefit
+            result['optimize_amount'] = purchase_num
+
+    print result['optimize_amount'], int(result['max_benefit'])
